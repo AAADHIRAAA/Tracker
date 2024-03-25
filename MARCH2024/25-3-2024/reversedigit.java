439b4cceaa1c90261b57fea382565b56e5820b9c
PADMAPRIYA <padmapriyas.2004@gmail.com>
2024-03-25 23:05:18 +0530

    Update reversedigit.java

--- MARCH2024/25-3-2024/reversedigit.java
+++ MARCH2024/25-3-2024/reversedigit.java
@@ -5,12 +5,12 @@ public class Main
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int num=input.nextInt();
-	  int rev=0;
-    while(num>0){
-      rev=num%10+rev;
-	    num/=10;
-      rev*=10;
-    }
-		System.out.println(rev);
-		}
+	  	int rev=0;
+    		while(num>0){
+      			rev=num%10+rev;
+	    		num/=10;
+      			rev*=10;
+    		}
+	       System.out.println(rev);
+	}
 }
