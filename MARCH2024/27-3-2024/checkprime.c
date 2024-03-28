ec5ac74346dd07f884c4e706dc09cd2762f90ad7
PADMAPRIYA <padmapriyas.2004@gmail.com>
2024-03-28 05:47:02 +0530

    Create checkprime.c

---
+++ MARCH2024/27-3-2024/checkprime.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+   int num, flag=0;
+   scanf("%d",&num);
+   for(int i=2; i<= num/2; ++i) {
+      if(num%i == 0) {
+         flag=1;
+         break;
+      }
+   }
+   if (num==1) {
+      printf("Neither Prime nor Composite");
+   }
+   else {
+       if (flag== 0)  printf("Prime");
+        else  printf("Not prime");
+      
+   }
+}
