1c251901a72f92ee29901ffa1f00c727cc7e0bb1
PADMAPRIYA <padmapriyas.2004@gmail.com>
2024-04-07 19:11:07 +0530

    Create 2nums.c

---
+++ MARCH2024/4-4-2024/2nums.c
@@ -0,0 +1,13 @@
+//check 2 nums are equal without bitwise operator
+#include<stdio.h>
+int main(){
+    int n1,n2;
+    scanf("%d",&n1);
+    scanf("%d",&n2);
+    if(n1-n2==0){
+        printf("numbers are equal");
+    }
+    else{
+        printf("numbers are not equal");
+    }
+}
